57ba2356b13e3854eb064a025e3620ac555b2a2d
Ian Hancock <northroadian@gmail.com>
2021-03-06 06:47:57 -0600

    react drills practice

--- src/Array2.js
+++ src/Array2.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Array2 extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            people: ['jim','dwight','creed','michael','pam','stanley']
-        };
-    }
-    render() {
-        let peopletoDisplay = this.state.people.map((element,index)=>{
-            return <h2 key= {index}> {element}</h2>
-        })
-        return (
-            <div className = 'Array2'> {peopletoDisplay}
-                
-            </div>
-        )
-    }
+function Array2() {
+
+    const products = [
+        'toothpaste',
+        'mouthwash',
+        'dental floss'
+    ]
+    return (
+        <div> 
+           {products.map((product)=>{
+               return <li key ={product}>{product}</li> 
+           })}
+        </div>
+    )
 }
+
+export default Array2
+
+ 
